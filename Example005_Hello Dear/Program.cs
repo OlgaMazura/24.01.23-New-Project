1e8805a91202a63b402d391320a2013a29935097
OlgaMazura <ofmazura@gmail.com>
2023-01-24 23:16:02 +0100

    Example005 HelloDear

---
+++ Example005_Hello Dear/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Type your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "olga")
+{
+    Console.WriteLine("Oh!!! It's Olga!!!");
+}
+else
+{
+    Console.Write("Hello ");
+    Console.WriteLine(username);
+}
