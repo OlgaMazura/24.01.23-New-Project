2324e18546cbdff7b5ba0c16def94fe6ad22cdcd
OlgaMazura <ofmazura@gmail.com>
2023-01-26 18:20:59 +0100

    Example007 Magic

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int xa = 5, ya = 5,
+    xb = 1, yb = 15,
+    xc = 20, yc = 15;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
